974d70620b14a7d5c5029e2e252af6692e7fb781
JettChenT <jettchen12345@gmail.com>
2025-12-07 16:30:13 -0500

    add analytics

--- platform/src/app/layout.tsx
+++ platform/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import Script from "next/script";
 import "./globals.css";
 import { Providers } from "@/components/providers";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
   title: "Curius Feed",
@@ -34,6 +35,7 @@ export default function RootLayout({
       </head>
       <body>
         <Providers>{children}</Providers>
+        <Analytics />
       </body>
     </html>
   );
